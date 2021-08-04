c04cc228b2a1686792b9d617e146dffc824c0915
nathanmurfy <nathanmurphyy02@gmail.com>
2021-08-04 13:37:54 +1000

    Added date function

--- src/com/trainingApp/TrainingTracker.java
+++ src/com/trainingApp/TrainingTracker.java
@@ -1,8 +1,12 @@
 package com.trainingApp;
 
 import java.io.File;
+import java.time.LocalDate;
 
 public class TrainingTracker {
     // Need to find a file path that works
     File fileCreator = new File("");
+
+    // Check if local dates line up
+    private LocalDate localDate = LocalDate.now();
 }
