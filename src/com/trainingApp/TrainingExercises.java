0729aaff0a25b54b7fd80465c22e931c2a12c22b
nathanmurfy <nathanmurphyy02@gmail.com>
2021-08-04 13:33:54 +1000

    Added file creator and variables

--- src/com/trainingApp/TrainingExercises.java
+++ src/com/trainingApp/TrainingExercises.java
@@ -1,4 +1,8 @@
 package com.trainingApp;
 
 public class TrainingExercises {
+    private String exercise;
+    private int weight;
+    private int reps;
+    private int sets;
 }
--- src/com/trainingApp/TrainingTracker.java
+++ src/com/trainingApp/TrainingTracker.java
@@ -1,4 +1,8 @@
 package com.trainingApp;
 
+import java.io.File;
+
 public class TrainingTracker {
+    // Need to find a file path that works
+    File fileCreator = new File("");
 }
