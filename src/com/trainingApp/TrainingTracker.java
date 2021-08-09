6c61025e328f82f37696ac3c03f88d60cc0c429d
nathanmurfy <nathanmurphyy02@gmail.com>
2021-08-09 14:16:24 +1000

    added IOexception with file stream

--- src/com/trainingApp/TrainingTracker.java
+++ src/com/trainingApp/TrainingTracker.java
@@ -1,9 +1,6 @@
 package com.trainingApp;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -20,25 +17,26 @@ public class TrainingTracker {
             userSelected = MenuData();
             switch(userSelected){
                 case 1:
-                    System.out.println("Chest--1");
+                    System.out.println("Chest - 1");
                     break;
                 case 2:
-                    System.out.println("Back--2");
+                    System.out.println("Back - 2");
                     break;
                 case 3:
-                    System.out.println("Legs--3");
+                    System.out.println("Legs - 3");
                     break;
                 case 4:
-                    System.out.println("Shoulders--4");
+                    System.out.println("Shoulders - 4");
                     break;
                 case 5:
-                    System.out.println("Arms--5");
+                    System.out.println("Arms - 5");
                     break;
                 default:
+                    System.out.println("Unexpected value");
                     break;
             }
         }
-        while(userSelected > 5);
+        while(userSelected >= 5);
     }
 
 
@@ -47,11 +45,11 @@ public class TrainingTracker {
         Scanner scanner = new Scanner(System.in);
         System.out.println("What are you training today?");
         System.out.println("----------------------------\n");
-        System.out.println("Chest -1");
-        System.out.println("Back -2");
-        System.out.println("Legs -3");
-        System.out.println("Shoulders -4");
-        System.out.println("Arms -5");
+        System.out.println("Chest - 1");
+        System.out.println("Back - 2");
+        System.out.println("Legs - 3");
+        System.out.println("Shoulders - 4");
+        System.out.println("Arms - 5");
 
         System.out.println("You selected body part number: ");
         selection = scanner.nextInt();
@@ -65,6 +63,32 @@ public class TrainingTracker {
             bufferedWriter.close();
         } catch (IOException e){
             e.printStackTrace();
+        }
+    }
+    public static class ExceptionExample {
+        public FileInputStream testMethod1(){
+            File file = new File("C:\\Users\\UST-User\\Desktop\\text.txt.txt");
+            FileInputStream fileInputStream = null;
+            try{
+                fileInputStream = new FileInputStream(file);
+                fileInputStream.read();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            finally{
+                try{
+                    if(fileInputStream != null) {
+                        fileInputStream.close();
+                    }
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return fileInputStream;
+        }
+        public static void main(String[] args){
+            ExceptionExample instance1 = new ExceptionExample();
+            instance1.testMethod1();
         }
     }
 
