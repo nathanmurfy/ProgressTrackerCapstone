24405701fa457e53a1ed1bfa7d312023465f2d96
nathanmurfy <nathanmurphyy02@gmail.com>
2021-08-10 11:55:52 +1000

    added comments

--- src/com/trainingApp/Main.java
+++ src/com/trainingApp/Main.java
@@ -1,10 +1,6 @@
 package com.trainingApp;
 
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
-
 import static com.trainingApp.TrainingTracker.DateAndTime;
 import static com.trainingApp.TrainingTracker.MenuData;
 
@@ -12,38 +8,50 @@ public class Main {
 
     public static void main(String[] args) {
 
-    	Scanner sc = new Scanner(System.in);
-		TrainingExercises trainingExercises = new TrainingExercises();
+        // Starting the scanner
+        Scanner sc = new Scanner(System.in);
 
-		DateAndTime();
-    	MenuData();
-	System.out.println("Welcome user!. This program helps track workout progress.");
-	System.out.println("Please enter what exercise you are doing: ");
-	trainingExercises.setExercise(sc.nextLine());
-	System.out.println("Please enter the weight: ");
-	trainingExercises.setWeight(sc.nextLine());
-	System.out.println("Please enter how many reps: ");
-	trainingExercises.setReps(sc.nextLine());
-	System.out.println("Enter how many sets: ");
-	trainingExercises.setSets(sc.nextLine());
+        // Initializing trainingExercises
+        TrainingExercises trainingExercises = new TrainingExercises();
 
-	String output = "";
-	int reps = Integer.parseInt(trainingExercises.getReps());
-	int sets = Integer.parseInt(trainingExercises.getSets());
-	output += "The exercise you did was: " + trainingExercises.getExercise() + "\n";
-	output += "You weight you did was: " + trainingExercises.getWeight() + " KG" + "\n";
-	output += "You amount of reps you did was: " + reps + " reps" + "\n";
-	output += "The amount of sets you did was: " + sets + " sets" + "\n";
-	output += "The total amount was: " + reps * sets + " reps" + "\n";
+        // Calling the DateAndTime and the MenuData class
+        DateAndTime();
+        MenuData();
 
-	System.out.println(output);
+        // Asking the user for input
+        System.out.println("Welcome user!. This program helps track workout progress.");
+        System.out.println("Please enter what exercise you are doing: ");
+        trainingExercises.setExercise(sc.nextLine());
+        System.out.println("Please enter the weight: ");
+        trainingExercises.setWeight(sc.nextLine());
+        System.out.println("Please enter how many reps: ");
+        trainingExercises.setReps(sc.nextLine());
+        System.out.println("Enter how many sets: ");
+        trainingExercises.setSets(sc.nextLine());
 
-	try {
-		System.out.println(TrainingTracker.printer(output));
-	} catch (Exception e){
-		System.out.println("error printing to file");
-		System.out.println("error was: " + e.getMessage());
-	}
+        // Initialising all console output to the "output" String
+        String output = "";
+
+        // Passing reps and sets as an int
+        int reps = Integer.parseInt(trainingExercises.getReps());
+        int sets = Integer.parseInt(trainingExercises.getSets());
+
+        // Output into the console
+        output += "The exercise you did was: " + trainingExercises.getExercise() + "\n";
+        output += "You weight you did was: " + trainingExercises.getWeight() + " KG" + "\n";
+        output += "You amount of reps you did was: " + reps + " reps" + "\n";
+        output += "The amount of sets you did was: " + sets + " sets" + "\n";
+        output += "The total amount was: " + reps * sets + " reps" + "\n";
+
+        System.out.println(output);
+
+        // Printing to console if the output successfully printed to the file
+        try {
+            System.out.println(TrainingTracker.printer(output));
+        } catch (Exception e) {
+            System.out.println("error printing to file");
+            System.out.println("error was: " + e.getMessage());
+        }
 
 
     }
--- src/com/trainingApp/TrainingTracker.java
+++ src/com/trainingApp/TrainingTracker.java
@@ -3,15 +3,13 @@ package com.trainingApp;
 import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class TrainingTracker {
 
     File fileCreator = new File("");
 
-
+    // Asking user what muscle group they are training
     public static int MenuData() {
         int selection;
         Scanner scanner = new Scanner(System.in);
@@ -23,27 +21,30 @@ public class TrainingTracker {
         System.out.println("Shoulders - 4");
         System.out.println("Arms - 5");
 
+        // Printing the body part they chose
         System.out.println("You selected body part number: ");
         selection = scanner.nextInt();
         return selection;
     }
-
+    // Formatting the date and time
     public static void DateAndTime() {
         LocalDate localDate = LocalDate.now();
+        // Changing the pattern to "dd, MONTH yyyy"
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy");
         String formattedString = localDate.format(formatter);
-
+        // Printing out the formatted date
         System.out.println(LocalDate.now().format(DateTimeFormatter.ofPattern("dd MMMM yyyy")));
     }
-
+    // This class is printing to the Output.txt file
     public static String printer(String output) throws IOException {
         String filenameStr = "Output.txt";
-
+        // Outputting the console output to the file
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("Output.txt"));
         bufferedWriter.write(output);
         bufferedWriter.close();
 
         return filenameStr;
     }
+
 }
 
