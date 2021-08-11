6e590d75cd56c4bee1632477a9aca4075f984649
nathanmurfy <nathanmurphyy02@gmail.com>
2021-08-11 13:26:48 +1000

    Made some layout changes

---
+++ src/test/java/com/trainingApp/TrainingTrackerTest.java
@@ -0,0 +1,12 @@
+package com.trainingApp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrainingTrackerTest {
+    @Test
+    public void localDateTest() {
+        TrainingTracker trainingTrackerTest = new TrainingTracker();
+    }
+}
